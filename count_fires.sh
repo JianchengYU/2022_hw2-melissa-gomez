7d5145f33d07ca4f229e1e5ed9155e6350f3a97f
melissa-gomez <114562104+melissa-gomez@users.noreply.github.com>
2022-10-12 00:41:33 -0700

    Update count_fires.sh

--- count_fires.sh
+++ count_fires.sh
@@ -1,32 +1,31 @@
-#!/bin/bash -l
+!/usr/bin/bash -l
 
-# step to write
-# download the CSV file
-# curl -o calfire.csv ...
+#download csv file  ( i did the github clone instead)
 
-# print out the range of years found -- you may need to go in and edit the file
-# cut -d, ....
-MINYEAR=1900
-MAXYEAR=1901
-# write code to set these variables with the smallest and largest years
-echo "This report has the years: $MINYEAR-$MAXYEAR"
-# if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
+curl  https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::recent-large-fire-perimeters-5000-acres.csv > calfires_2021.csv
+git clone https://github.com/biodataprog/2022_hw2-melissa-gomez
 
-# print out the total number of fires (remember to remove the header line)
-TOTALFILECOUNT=0
-# put your code here to update this variable
-echo "Total number of files: $TOTALFILECOUNT"
+#print out the range of years that occur in this file (hint cut out the column you want, sort and get either the smallest or largest number
+cut -d, -f2 calfires_2021.csv | sort -n
 
-# print out the number of fire in each year
-echo "Number of fires in each year follows:"
+#print out the number of fires in the database
+cut -d, -f2 calfires_2021.csv | sort -g | wc -l
+
+#remove header
+cut -d, -f2 calfires_2021.csv | sort -g | tail -n +2 | wc -l
+
+#Print out the number of fires that occur each year
+cut -d, -f2 calfires_2021.csv | sort -g | uniq -c
+cut -d, -f2 calfires_2021.csv | sort -g | tail -n +2 | wc -l
 
 
-# print out the name of the largest file use the GIS_ACRES and report the number of acres
-echo "Largest fire was $LARGEST and burned $LARGESTACRES"
+#Print out the name largest fire
+awk -F, '{print $13,$2,$6}' calfires_2021.csv | sort -n | tail -n 1
 
-# print out the years - change the code in $(echo 1990) to print out the years (hint - how did you get MINYEAR and MAXYEAR?
-for YEAR in $(echo 1990)
-do
-#      TOTAL=$(grep ... | awk ...)
+#Print out the total acreage burned in each year
+
+for YEAR in 2017 2018 2019 2020 2021
+   do
+      TOTAL=$(grep $YEAR calfires_2021.csv | awk -F',' '{sum+=$13;} END{print sum;}')
       echo "In Year $YEAR, Total was $TOTAL"
-done
+   done
